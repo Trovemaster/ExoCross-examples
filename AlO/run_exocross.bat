5f267a0ecb065ec4e58a75feb4d58827ecd80528
Sergey Yurchenko <sergei.yurchenko@gmail.com>
2019-01-26 22:55:39 +0000

    First commit

---
+++ AlO/run_exocross.bat
@@ -0,0 +1,7 @@
+REM Example of a Windows batch file
+REM Copy xcross_dos.exe to the folder containing the input
+REM Edit the command below: change the names of the input/output files
+REN Open the window containing the working folder and execute run_exocross.bat
+xcross_dos.exe < AlO_Voigt_2000K.inp> AlO_Voigt_2000K.out
+
+
---
+++ H2O/run_exocross.bat
@@ -0,0 +1,7 @@
+REM Example of a Windows batch file
+REM Copy xcross_dos.exe to the folder containing the input
+REM Edit the command below: change the names of the input/output files
+REN Open the window containing the working folder and execute run_exocross.bat
+xcross_dos.exe < H2O_Voigt_T1000K.inp> H2O_Voigt_T1000K.out
+
+
